b55a5c4aed24f636f8daaa670954ce4105fbcbab
Nisa Ayaz <nnisayazz@gmail.com>
2026-01-06 19:55:17 +0300

    commit

---
+++ src/reducer/postsReducer.js
@@ -0,0 +1,14 @@
+export const initialState = {
+    posts: [],
+    loading: false,
+    error: null
+}
+
+export function postsReducer(state, action) {
+    switch(action.type) {
+        case "FETCH_START": return {...state, loading: true, error: null};
+        case "FETCH_SUCCESS": return {...state, loading: false, posts: action.payLoad};
+        case "FETCH_ERROR": return {...state, loading: false, error: action.payLoad};
+        default: return state;
+    }
+}
