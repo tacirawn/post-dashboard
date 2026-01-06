0ba46e0cbba79882df3a67df50534ba047b6afb3
Nisa Ayaz <nnisayazz@gmail.com>
2026-01-06 20:12:58 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useReducer} from 'react';
+import fetchPosts from './api/postsApi';
+import { postsReducer, initialState } from './reducer/postsReducer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(postsReducer, initialState);
+  useEffect = () => {
+    async function load() {
+      dispatch({type: "FETCH_START"});
+      try {
+      const data = await fetchPosts();
+      dispatch({type: "FETCH_SUCCESS", payload: data});
+      } catch (error) {
+      dispatch({type: "FETCH_ERROR", payload: error.message});
+      }
+    }
+    load();
+  }, []; 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h1>Post Dashboard</h1>
+    
+  );
 }
 
-export default App
+export default App;
